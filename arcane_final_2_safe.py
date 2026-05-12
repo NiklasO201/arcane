ce5d28d5acc41170dcd958361b2f9095988bd07c
NiklasO201 <oppitzniklas352@gmail.com>
2026-05-12 17:26:13 +0200

    Update arcane_final_2_safe.py

--- arcane_final_2_safe.py
+++ arcane_final_2_safe.py
@@ -225,5 +225,5 @@ def get_archive(): return jsonify(load_json('archiv.json'))
 
 if __name__ == '__main__':
     print("A.R.C.A.N.E. Server laeuft auf http://localhost:5000")
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    port = int(os.environ.get("PORT", 8080))
+app.run(host="0.0.0.0", port=port, debug=False)
